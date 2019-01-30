b3305085fcfa61882299e72f8a75e7081bc3e74a
William Figliolini <William_Figliolini@student.uml.edu>
2019-01-30 11:14:53 -0500

    S3.3 Added final Tests Moved testing suite into separate file

---
+++ R0/R0Test.h
@@ -0,0 +1,11 @@
+//R0Test.h
+#ifndef R0_TEST_H_
+#define R0_TEST_H_
+
+#include "R0/R0.h"
+#include <boost/test/unit_test.hpp>
+
+int RTest();
+
+#endif
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-#include "R0/R0.h"
-#include <stack>
-#include <boost/test/unit_test.hpp>
 
-int RTest();
+#include "R0/R0Test.h"
 
 int main(int argc, char* argv) {
 	int status = RTest();
@@ -12,56 +9,4 @@ int main(int argc, char* argv) {
 	else {
 		std::cout << "Tests Failed with code " << status << std::endl;
 	}
-}
-
-int RTest()
-{
-	int status = 0;
-	//init stack for reads
-	//testing
-	Program* R0Test1 = new Program();
-	R0Test1->setExpr(new Neg(new Add(new Num(17), new Add(new Read(1), new Num(42)))));
-	numgen.pop();
-	std::string temp1, result1("(-(+17 (+(Read) 42)))");
-	int  int1, final1(-101);
-
-	Program* R0Test2 = new Program(NULL, new Add(new Num(7), new Num(13)));
-	std::string temp2, result2("(+17 13)");
-	int  int2, final2(20);
-
-	auto R0TestNulls = new Program();
-	Program* testRead(NULL, new Read());
-	// setting up for first set of tests
-	test1 = R0Test1->print();
-	int1 = R0Test1->run();
-
-	BOOST_AUTO_TEST_CASE(AST_Test1) {
-		BOOST_REQUIRE(temp1.compare(result1) == 0);
-	};
-
-
-	BOOST_AUTO_TEST_CASE(Inter_Test1) {
-		BOOST_REQUIRE(int1 == final1);
-	};
-	test2 = R0Test2->print();
-	int2 = R0Test2->run();
-	BOOST_AUTO_TEST_CASE(AST_Test2) {
-		BOOST_REQUIRE(temp2.compare(result2) == 0);
-	};
-	BOOST_AUTO_TEST_CASE(Inter_Test2) {
-		BOOST_REQUIRE(int2 == final2);
-	};
-	BOOST_AUTO_TEST_CASE(Null_Test) {
-		BOOST_REQUIRE(R0TestNulls->getInfo() == NULL);
-		BOOST_REQUIRE(R0TestNulls->getExpr() == NULL);
-	};
-
-	//read test, needs user input & cant be automated to my knowledge
-
-	int out;
-
-	out = testRead->run();
-	std::cout << out << endl;
-
-	return status;
 }
