dbcb14854112ad565011c52f67e8913cf39b978d
Bill <william_figliolini@student.uml.edu>
2019-02-25 18:37:47 -0500

    Completed step 8 by testing all major cases

--- R0/RTest.cpp
+++ R0/RTest.cpp
@@ -13,19 +13,13 @@ BOOST_AUTO_TEST_SUITE(R0TESTS)
 	BOOST_AUTO_TEST_CASE(Test1){ 
 	        Program* R0Test1 = new Program();
 	        R0Test1->setExpr(new Neg(new Add(new Num(17), new Add(new Read(1), new Num(42)))));
-	        std::string temp1, result1("(-(+17 (+(Read) 42)))");
-	        int  int1, final1(-101);
-	        temp1 = R0Test1->print();		
+	        int  int1, final1(-101);		
 	        int1 = R0Test1->run();
-		BOOST_REQUIRE(temp1 == result1);
 		BOOST_REQUIRE(int1 == final1);
 	}
-	//test for optimizations
 	BOOST_AUTO_TEST_CASE(Test2) {
 	        Program* R0Test2 = new Program(NULL, new Add(new Num(7), new Num(13)));
-	        std::string temp2, result2("(+7 13)");
 	        int  int2, final2(20);
-		temp2 = R0Test2->print();
 	        int2 = R0Test2->run();
 		BOOST_REQUIRE(temp2 == result2);
 		BOOST_REQUIRE(int2 == final2);
@@ -33,21 +27,16 @@ BOOST_AUTO_TEST_SUITE(R0TESTS)
 	// test for two branches with reads
 	BOOST_AUTO_TEST_CASE(Test3) {
 		Program* R0Test3 = new Program(NULL, new Add(new Add(new Num(7), new Read(1)), new Add(new Num(13), new Read(1))));
-		std::string temp3, result3("(+(+7 (Read)) (+13 (Read)))");
 		int  int3, final3(101);
-		temp3 = R0Test3->print();
 		int3 = R0Test3->run();
-		BOOST_REQUIRE(temp3 == result3);
 		BOOST_REQUIRE(int3 == final3);
 	}
 
 	BOOST_AUTO_TEST_CASE(Test4) {
 		Program* R0Test4 = new Program(NULL, new Neg(new Num(7)));
-		std::string temp4, result4("(-7)");
 		int  int4, final4(-7);
 		temp4 = R0Test4->print();
 		int4 = R0Test4->run();
-		BOOST_REQUIRE(temp4 == result4);
 		BOOST_REQUIRE(int4 == final4);
 	}
 
@@ -75,7 +64,7 @@ BOOST_AUTO_TEST_SUITE(R0TESTS)
 		Program* pTest = randProg(0);
 		//std::cout <<" run prog" << std::endl;
 		result = pTest->run();
-		std::cout << "program generated, result: "<< result << std::endl;
+		//std::cout << "program generated, result: "<< result << std::endl;
 		BOOST_TEST(true);
 	}
 
@@ -86,21 +75,49 @@ BOOST_AUTO_TEST_SUITE(R0TESTS)
 		//std::cout << "generating program of depth 10 ";
 		Program* pTest = randProg(10);
 		result = pTest->run();
-		std::cout << "program generated, result: " << result << std::endl;
+		//std::cout << "program generated, result: " << result << std::endl;
 		BOOST_TEST(true);
 	}
 	// Depth 100 results in system stalling and losing the rest of the file 
 	BOOST_AUTO_TEST_CASE(Mass_Test){
                 int depth(10), runCount(100);
-		std::cout << "generating "<< runCount << " programs of depth " << depth << std::endl;
+		//std::cout << "generating "<< runCount << " programs of depth " << depth << std::endl;
                 Program* pTest;
 		for(int i = 0; i< runCount; ++i){
 			int result;
 			pTest = randProg(depth);
                 	result = pTest->run();
-                	std::cout << "program generated, result: " << result << std::endl;
+                	//std::cout << "program generated, result: " << result << std::endl;
 		}
                 BOOST_TEST(true);
         }
+	// Optimizer cases
+	// Addition Case
+	BOOST_AUTO_TEST_CASE(OPT1){
+		Program* pTest = new Program(NULL, new Add(new Num(2), new Num(3)));
+		int result(5), tempInt;
+		std::string AST("5"), tempStr;
+		tempStr = pTemp->print();
+		tempInt = pTemp->run();
+		BOOST_REQUIRE( result == tempInt);
+		BOOST_REQUIRE( AST == tempStr);
+	}
+	//negative case
+	BOOST_AUTO_TEST_CASE(OPT2){
+		Program* pTest = new Program(NULL, new Neg(new Neg(new Num(7))));
+		int result(7), tempInt;
+		std::string AST("7"), tempStr;
+		tempStr = pTemp->print();
+		tempInt = pTemp->run();
+		BOOST_REQUIRE( result == tempInt);
+		BOOST_REQUIRE( AST == tempStr);
+	}
+	//Addition with read
+	BOOST_AUTO_TEST_CASE(OPT1){
+		Program* pTest = new Program(NULL, new Add(new Num(7), new Add( new Read(1), new Num(3))));
+		std::string AST("(+ 10 Read)"), tempStr;
+		tempStr = pTemp->print();
+		BOOST_REQUIRE( result == tempInt);
+	}
 
 BOOST_AUTO_TEST_SUITE_END()
