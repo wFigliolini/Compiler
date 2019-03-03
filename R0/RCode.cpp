0714896e67a160efcd7e6b02a37876cc4f6a528d
Bill <william_figliolini@student.uml.edu>
2019-03-03 13:17:41 -0500

    Completed step 13 by adding let to randExpr

--- R0/RCode.cpp
+++ R0/RCode.cpp
@@ -2,6 +2,7 @@
 //Contains function definition of Tests
 
 #include "RCode.h"
+#include <list>
 
 Expr* powE(int x, int b){
 	if(x == 1){
@@ -21,39 +22,74 @@ Program* pow(int x, int b ){
  //choiceGen to determine path
  //numGen to determine num values
  
- std::mt19937 choiceGen, numGen;
+ std::mt19937 choiceGen, numGen, stringGen;
  
  //associated distributions
  
- std::uniform_int_distribution<int> choiceDis(0,1), numDis(0,1024);
+ std::uniform_int_distribution<int> choiceDis(0,2), numDis(0,1024), stringDis(0,61);
 
-
-Expr* randExpr(int depth){
+typedef std::vector<std::string> vars;
+std::string randString(int len);
+ 
+Expr* randExpr(int depth, vars env){
 	int choice =  choiceDis(choiceGen);
 	//std::cout << "choice: " << choice << std::endl;
 	if(depth <= 0){
-		if( choice == 1 ){
+		if( choice == 0 ){
 			return new Read(1);
 		}
-		else{
+		else if(choice == 1){
             int i(numDis(numGen));
 			Num* num = new Num(i);
 			return num;
-		}
+		}else{
+            int i(numDis(numGen));
+            //generate var
+            if(env.empty()) {
+                //fallback if no lets is to act as a number
+                //will result in uneven distributions prior to first let, after should have 33% chance of any 
+                return new Num(i);
+            }
+            else{
+                //use i to randomly select from set of vars
+                std::string out = env[i%env.size()];
+                return new Var(out);
+            }
+            
+        }
 	}
 	else{
 		int nextDepth= depth-1 ;
-		if( choice == 1 ){
-			return new Neg( randExpr(nextDepth) );
+		if( choice == 0 ){
+			return new Neg( randExpr(nextDepth, env) );
+		}
+		else if(choice == 1){
+			return new Add( randExpr(nextDepth, env), randExpr(nextDepth, env) );
 		}
 		else{
-			return new Add( randExpr(nextDepth), randExpr(nextDepth) );
-		}
+            //generate let
+            std::string name = randString(10);
+            vars env2(env);
+            env2.push_back(name);
+            return new Let(name, randExpr(nextDepth, env), randExpr(nextDepth,env2)); 
+        }
 	}
 }
 
+std::string randString(int len){
+    int randNum;
+    std::string result;
+    static const char alphaNum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for(int i = 0; i < len; ++i){
+        randNum = stringDis(stringGen);
+        result += alphaNum[randNum];
+    }
+    return result;
+}
+
 Program* randProg(int depth){
-	Expr* val = randExpr(depth);
+    vars env;
+	Expr* val = randExpr(depth, env);
 	Program* ret = new Program(NULL, val);
 	//std::cout << "returning prog" << std::endl;
 	return ret;
