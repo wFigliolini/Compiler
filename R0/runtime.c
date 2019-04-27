47c61cc3e27efdae7b1bea83f730dd1b4f667f03
Bill <william_figliolini@student.uml.edu>
2019-04-27 13:04:27 -0400

    Attempted to fix segfault

--- R0/runtime.c
+++ R0/runtime.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int read_int(){
+int _read_int(){
     int x;
     scanf("%d", &x);
     return x;
 }
-int print_int(int x){
+int _print_int(int x){
     printf("%d\n", x);
     return 0;
 }
